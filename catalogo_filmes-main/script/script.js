af11df9821d253a97a488ee0e1d9f1feba497f99
kayque <clara.moreira.rodrigues@escola.pr.gov.br>
2026-06-19 11:19:17 -0300

    Add files via upload

--- catalogo_filmes-main/script/script.js
+++ catalogo_filmes-main/script/script.js
@@ -1,35 +1,8 @@
-const filmes = [{
-    titulo: "Vingadores",
-    imagem: "https://br.web.img2.acsta.net/pictures/18/03/16/15/08/2019826.jpg",
-    descricao: "Os maiores heróis da terra unidos",
-},
-{
-    titulo: "Meu amigo Totoro",
-    imagem: "https://static.wikia.nocookie.net/dublagem/images/f/f6/Meu_Amigo_Totoro.jpg/revision/latest?cb=20241127000905&path-prefix=pt-br",
-    descricao: "Uma jornada de inocência e magia",
-},
-{
-    titulo: "Homem Aranha",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/5/53/The_Amazing_Spider-Man_Poster.jpg",
-    descricao: "O amigo da visinhança",
-},
-{
-    titulo: "Super Mario Bros",
-    imagem: "https://www.universalpics.com.br/tl_files/content/movies/super_mario_bros/posters/01.jpg",
-    descricao: "Os irmãos encanadores Mario e Luigi",
-},
-{
-    titulo: "Pokémon: Mewtwo contra-ataca",
-    imagem: "https://play-lh.googleusercontent.com/piOEZWvgVx8-xj3K8uwdpVZ_ttnicstSKDnKrRxnLLOKEIGiHjmYqZxCsjnGj85Uu0cQmRHRgz4StOUzAiA",
-    descricao: "O primeiro filme de pokémon remasterizado em CGI",
-},
-{
-    titulo: "Ponyo",
-    imagem: "https://m.media-amazon.com/images/M/MV5BMTkwYjBhOWItOWM1ZC00N2MyLTg2ZGQtNWYxNjE3ZDk4YmQ1XkEyXkFqcGc@._V1_.jpg",
-    descricao: "Uma Amizade que Veio do Mar",
+async function carregarFilmes() {
+    const resposta = await fetch("https://localhost:5000/filmes")
+    const filmes = await resposta.json()
+    mostrarFilmes(filmes)
 }
-
-]
 
 const listaFilmes = document.getElementById("listaFilmes");
 
@@ -51,7 +24,8 @@ function mostrarFilmes(lista) {
     })
 }
 
-mostrarFilmes(filmes)
+carregarFilmes()
+
 function verDetalhes(titulo) {
     Swal.fire({
         title: titulo,
